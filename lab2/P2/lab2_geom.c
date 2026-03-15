1661a41d1a03cf56ef52634ba6e1d6c1c06e592b
Maria Koltakova <kol06m@yandex.ru>
2026-03-15 15:31:31 +0300

    add solution lab P2

---
+++ lab2/P2/lab2_geom.c
@@ -0,0 +1,128 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include "lab2.h"
+
+static XVertex vertex[NVERT];
+static XFace face[(NFACE + 1)];
+static XEdge edge[NEDGE];
+
+static XPoint face3[NF3][(3 + 1)];
+static XPoint face4[NF4][(4 + 1)];
+static XPoint scale;
+
+int assoc(XPolyGraph* pg) {
+	pg->vertex = vertex;
+	pg->edge = edge;
+	pg->face = face;
+	return(0);
+}
+
+int resize(unsigned w, unsigned h) {
+	static XRectangle bak = { 0, 0, 0, 0 };
+	if ((bak.width == w) && (bak.height == h))
+		return(0);
+	bak.width = w; bak.height = h;
+	return(NFACE);
+}
+
+int rescale(unsigned w, unsigned h) {
+	int x, y;
+	x = w / NUNIT; y = h / NUNIT;
+	if ((scale.x == x) && (scale.y == y))
+		return(0);
+	scale.x = x; scale.y = y;
+	return(NFACE);
+}
+
+int rebuild() {
+	static XPoint vconf[] = {
+		{0, 0},   // 0
+		{0, 12},  // 1
+		{10, 6},  // 2
+		{2, 3},   // 3
+		{2, 9},   // 4
+		{7, 6}    // 5
+	};
+	static int fconf3[NF3][(3 + 1)] = {
+		{3, 5, 4, 3}   // внутренний треугольник
+	};
+	static int fconf4[NF4][(4 + 1)] = {
+		{0, 3, 4, 1, 0},  // четырёхугольник 1
+		{1, 4, 5, 2, 1},  // четырёхугольник 2
+		{2, 5, 3, 0, 2}   // четырёхугольник 3
+	};
+	static int econf[NEDGE][2] = {
+		{0, 1}, {1, 2}, {2, 0},  // внешний треугольник
+		{3, 4}, {4, 5}, {5, 3},  // внутренний треугольник
+		{0, 3}, {1, 4}, {2, 5}   // соединительные рёбра
+	};
+	int i, j;                    
+	for (i = 0; i < NVERT; i++) { 
+		vertex[i].x = scale.x * vconf[i].x;
+		vertex[i].y = scale.y * vconf[i].y;
+	} 
+	for (i = 0; i < NEDGE; i++) { 
+		edge[i].x1 = vertex[econf[i][0]].x;
+		edge[i].y1 = vertex[econf[i][0]].y;
+		edge[i].x2 = vertex[econf[i][1]].x;
+		edge[i].y2 = vertex[econf[i][1]].y;
+	} 
+	for (i = 0; i < NF3; i++)             
+		for (j = 0; j < (3 + 1); j++) {   
+			face3[i][j].x = vertex[fconf3[i][j]].x;
+			face3[i][j].y = vertex[fconf3[i][j]].y;
+		} 
+	
+	for (i = 0; i < NF4; i++)             
+		for (j = 0; j < (4 + 1); j++) {   
+			face4[i][j].x = vertex[fconf4[i][j]].x;
+			face4[i][j].y = vertex[fconf4[i][j]].y;
+		} 
+	return(0);
+}
+
+int retrace() {
+	int i = 0;
+	int j;
+	for (j = 0; j < NF3; j++, i++) {
+		face[i].top = face3[j];
+		face[i].Cn = 3;
+		face[i].tone = DEFTONE;
+		face[i].regi = XCreateRegion();
+	}
+
+	for (j = 0; j < NF4; j++, i++) {
+		face[i].top = face4[j];
+		face[i].Cn = 4;
+		face[i].tone = DEFTONE;
+		face[i].regi = XCreateRegion();
+	}
+
+	return(0);
+}
+
+int reconf(unsigned w, unsigned h) {
+	if (resize(w, h) == 0)
+		return(0);
+	if (rescale(w, h) != 0)
+		rebuild();
+	return(NFACE);
+}
+
+int zotone(int x, int y) {
+	static XPoint past = { 0, 0 }; 
+	int f = 0;                  
+	if ((past.x == scale.x) && (past.y == scale.y))
+		f = NFACE;    
+	for (; f < NFACE; f++) {    
+		XDestroyRegion(face[f].regi);
+		face[f].regi = XPolygonRegion(face[f].top, face[f].Cn, 0);
+	} 
+	past.x = scale.x;
+	past.y = scale.y;          
+	for (f = 0; f < NFACE; f++) 
+		if (XPointInRegion(face[f].regi, x, y) == True)
+			break;
+	face[f].tone = (face[f].tone + 1) % NCOLOR;   
+	return(f);                  
+}
